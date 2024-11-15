a9edc772af12927b89c920648eb252651841fbf9
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-11-15 14:40:46 +0200

    update social card

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/ad.svg' }
   ],
   htmlAttrs: {
     lang: 'en'
