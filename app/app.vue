59e7b3151c51bd63fac19458462f62e037c3507c
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-11-15 14:27:26 +0200

    update social card

--- app/app.vue
+++ app/app.vue
@@ -24,8 +24,8 @@ useHead({
 useSeoMeta({
   titleTemplate: `%s - ${seo?.siteName}`,
   ogSiteName: seo?.siteName,
-  ogImage: 'https://docs-template.nuxt.dev/social-card.png',
-  twitterImage: 'https://docs-template.nuxt.dev/social-card.png',
+  ogImage: 'https://docs-template.nuxt.dev/social.png',
+  twitterImage: 'https://docs-template.nuxt.dev/social.png',
   twitterCard: 'summary_large_image'
 })
 
