0e2db4b06cd26e179bb6ab6528d6f9d91d29b171
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-11-22 12:16:02 +0200

    task check list

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       fontFamily: {
-        sans: ['DM Sans', ...defaultTheme.fontFamily.sans]
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
       },
       colors: {
         brand:{
